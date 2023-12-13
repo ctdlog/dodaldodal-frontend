e18822a6c348b8bbe38cfa46705f29ef015ce01a
ctdlog <qpflapffhs76@gmail.com>
2023-12-13 15:19:36 +0000

    🚧 Home UI

--- app/(route)/home/layout.tsx
+++ app/(route)/home/layout.tsx
@@ -1,10 +1,12 @@
+import BottomNavigation from '@/app/_components/shared/bottom-navigation'
 import Header from '@/app/_components/shared/header'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main>
+    <main className='h-full bg-home-background bg-cover bg-center bg-no-repeat'>
       <Header />
       {children}
+      <BottomNavigation />
     </main>
   )
 }
